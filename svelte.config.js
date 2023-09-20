d8accc2fe2c0696abe366aea26e28bc6abe4e248
Henry Leung Kwan Wong <henry.lk.wong@asl.Local>
2023-09-20 14:40:24 +0800

    Update routing and fix bugs

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +8,28 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+//			base: process.env.NODE_ENV === 'production' ? '/kurosakirito.github.io' : ''
+//			base: ''
+		}
+
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: '/kurosakirito.github.io'
+		}
+	}
+};
+
+//export default config;
